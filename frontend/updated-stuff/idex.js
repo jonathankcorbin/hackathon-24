1cbb0392a1a367f55df552ae04f5cbe589df1753
officerbob <162031491+officerbob@users.noreply.github.com>
2024-03-03 14:15:00 -0500

    Added dark mode

--- frontend/updated-stuff/idex.js
+++ frontend/updated-stuff/idex.js
@@ -1,4 +1,12 @@
-function myFunction() {
-    var element = document.button;
-    element.classList.toggle("dark-mode");
-  }
+const toggleSwitch = document.querySelector('#darkModeToggle');
+
+// Add event listener for the dark mode toggle
+toggleSwitch.addEventListener('change', switchTheme, false);
+
+function switchTheme() {
+    if (toggleSwitch.checked) {
+        document.body.classList.add('dark-theme');
+    } else {
+        document.body.classList.remove('dark-theme');
+    }
+}
